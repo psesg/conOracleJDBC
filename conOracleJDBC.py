1248ffa3145655b21e5dfaa3356a241ff5a9436c
Serg <pseesp@mail.ru>
2022-04-15 00:11:14 +0300

    WinOK

--- conOracleJDBC.py
+++ conOracleJDBC.py
@@ -1,13 +1,29 @@
-import pandas
+
 import jaydebeapi
-conn = jaydebeapi.connect(
-'oracle.jdbc.driver.OracleDriver',
-'jdbc:oracle:thin:demipt2/peregrintook@de-oracle.chronosavant.ru:1521/deoracle',
-['demipt2’,’peregrintook'],
-'C:\sqldeveloper\jdbc\lib\ojdbc8.jar'
-#'/home/demipt2/ojdbc8.jar'
-)
+import pandas as pd
+
+
+dirver = 'oracle.jdbc.driver.OracleDriver'
+jarFile = r'C:\sqldeveloper\jdbc\lib\ojdbc8.jar'
+addr_ = 'de-oracle.chronosavant.ru' + ':' + '1521' + '/' + 'deoracle'
+url = 'jdbc:oracle:thin:@' + addr_
+print('url', url)
+DBUser = 'demipt2'
+DBPwd = 'peregrintook'
+conn = jaydebeapi.connect(dirver, url, [DBUser, DBPwd], jarFile)
+#sql_str = "select 'oracle' from dual"
+sql_str = "select ID_MODEL, NAME_MODEL, YEAR_ISSUE from pana_models"
+#df = pd.read_sql_query(sql_str, conn)
+#df.head()
+#print(df)
 curs = conn.cursor()
+curs.execute(sql_str)
 
+df = pd.DataFrame(curs.fetchall(), columns=['ID_MODEL', 'NAME_MODEL', 'YEAR_ISSUE'])
+#df.reset_index(drop=True, inplace=True)
+#df.index = df['ID_MODEL']
 
-# conn = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver', ['jdbc:oracle:thin:@hostname:1521:orcl', "username", "password"],"C:\\Users\\user\\Documents\\Drivers\\ojdbc6.jar")
+print(df)
+#print(curs.fetchall())
+curs.close()
+conn.close()
